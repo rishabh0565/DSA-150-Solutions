2f345888d35343d055063d5685676f820ee1ebe5
rishabh0565 <rishabhsrivastava0565@gmail.com>
2025-06-03 03:58:04 +0530

    Copy Linked List with Random Pointer

---
+++ src/LinkedList/CopyLinkedListWithRandomPointer.java
@@ -0,0 +1,51 @@
+package LinkedList;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+//ou are given the head of a linked list of length n. Unlike a singly linked list, each node contains an additional pointer random, which may point to any node in the list, or null.
+//
+//Create a deep copy of the list.
+//
+//The deep copy should consist of exactly n new nodes, each including:
+//
+//The original value val of the copied node
+//A next pointer to the new node corresponding to the next pointer of the original node
+//A random pointer to the new node corresponding to the random pointer of the original node
+//Note: None of the pointers in the new list should point to nodes in the original list.
+//
+//Return the head of the copied linked list.
+//
+//In the examples, the linked list is represented as a list of n nodes. Each node is represented as a pair of [val, random_index] where random_index is the index of the node (0-indexed) that the random pointer points to, or null if it does not point to any node.
+//
+//Example 1:
+//
+//
+//
+//Input: head = [[3,null],[7,3],[4,0],[5,1]]
+//
+//Output: [[3,null],[7,3],[4,0],[5,1]]
+public class CopyLinkedListWithRandomPointer {
+	   public Node copyRandomList(Node head) {
+		    Node curr = head;    
+		     HashMap <Node, Node> nodeMap = new HashMap<>(); 
+		     while(head!=null){
+		          Node newNode = new Node(head.val); 
+		          nodeMap.put(head, newNode); 
+		          head = head.next; 
+		     }
+
+		    
+		    for(Map.Entry<Node,Node> entry : nodeMap.entrySet()){
+		         Node newNode  = entry.getValue();
+		         Node old = entry.getKey(); 
+		        newNode.next = nodeMap.get(old.next); 
+		         newNode.random = nodeMap.get(old.random);  
+		    }
+
+		    return nodeMap.get(curr); 
+
+		        
+		    }
+}
--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -3,9 +3,26 @@ package LinkedList;
 
 public class Main {
   public static void main(String[] args) {
-	   RemoveNodeFromEndOfLinkedList rnfroll = new RemoveNodeFromEndOfLinkedList(); 
-	   ListNode head  = createLinkedList(new int[] {5}); 
-	  display( rnfroll.removeNthFromEnd(head , 1));
+	   CopyLinkedListWithRandomPointer cllwrp = new CopyLinkedListWithRandomPointer(); 
+	   Node head = new Node(3); 
+	   head.next = new Node(7); 
+	   head.random = null; 
+	   Node second = head.next; 
+	   second.next = new Node(4); // second random remaining; 
+	   
+	   Node third = second.next; 
+	   third.next = new Node(5); 
+	   third.random = head; 
+	   
+	   Node fourth = third.next; 
+	   fourth.next = null; 
+	   fourth.random = second; 
+	   
+	   second.random = fourth; 
+	   // Node.display(head);
+	   Node resHead = cllwrp.copyRandomList(head); 
+	   Node.display(resHead);
+	   
    }
   
   
@@ -28,3 +45,37 @@ public class Main {
 	    }
  }
 }
+////////////////////////////////////////////  
+//  used for question 
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+    public static Node createLinkedList(int [] arr) {
+    	Node dummy  = new Node(0); 
+    	Node current  = dummy; 
+    	  for(int a : arr) {
+    		  current.next = new Node(a); 
+    		  current = current.next; 
+    	  }
+    	  return dummy.next; 
+    }
+    
+    public static void display(Node head) {
+  	  System.out.println();
+  	Node curr = head ; 
+  	    while(curr!=null) {
+  	    	 System.out.print("Node : "+   curr.val + " Next -> "  +  ((curr.next == null) ? "null" : curr.next.val) + "-> Random -> " + ((curr.random == null) ? "null" : curr.random.val) );
+  	    	 System.out.println();
+  	    	 curr = curr.next; 
+  	    }
+   }
+}
+
+
